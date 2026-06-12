5afc5779428f8673e972a89d7daba0b729dbbfe1
Or Baba <orbaba@Ors-MacBook-Air.local>
2026-06-12 17:34:59 +0800

    fix supabase package and remove hardcoded keys

--- app/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://bqqlgibvcyyoxwwqvusb.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxcWxnaWJ2Y3l5b3h3d3F2dXNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NzYzNDYsImV4cCI6MjA5NTQ1MjM0Nn0.e8eQqq48l2_qCA_D3eCRG3iVEuIZeOAyMRasl5rUFWY'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
